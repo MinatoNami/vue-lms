d6e388d83696bbce3cba457dd84eb4074bd54f01
MinatoNami <minato.nami.dev@gmail.com>
2024-01-23 21:28:44 +0800

    Clean up files and add bulma + axios

--- lmsnet_vue/src/main.ts
+++ lmsnet_vue/src/main.ts
@@ -5,10 +5,15 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+
+// import '@/style.css'
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000/'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+// app.use(axios)
 app.mount('#app')
