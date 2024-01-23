c62d371702676be744396b9e3d0b0290cd829e4a
Lionel <txg_ace@photons.com.tw>
2024-01-23 23:09:22 +0800

    Create login page and logout feature

--- lmsnet_vue/src/views/LoginView.vue
+++ lmsnet_vue/src/views/LoginView.vue
@@ -9,18 +9,19 @@
       <div class="container">
         <div class="column">
           <div class="column is-4 is-offset-4">
-            <form>
+            <form v-on:submit.prevent="submitForm">
               <div class="field">
                 <label>Email</label>
-                <div class="control"><input type="email" class="input" /></div>
+                <div class="control"><input type="email" class="input" v-model="username" /></div>
               </div>
               <div class="field">
                 <label>Password</label>
-                <div class="control"><input type="password" class="input" /></div>
+                <div class="control">
+                  <input type="password" class="input" v-model="password" />
+                </div>
               </div>
-              <div class="field">
-                <label>Confirm password</label>
-                <div class="control"><input type="password" class="input" /></div>
+              <div class="notification is-danger" v-if="errors.length">
+                <p v-for="error in errors" :key="error">{{ error }}</p>
               </div>
               <div class="field">
                 <div class="control">
@@ -39,6 +40,70 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useUserStore } from '@/stores'
+import axios from 'axios'
+import { ref, type Ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const store = useUserStore()
+
+const username = ref('')
+const password = ref('')
+
+const errors: Ref<string[]> = ref([])
+
+const submitForm = () => {
+  axios.defaults.headers.common['Authorization'] = ''
+
+  localStorage.removeItem('token')
+
+  errors.value = []
+  if (username.value === '') {
+    errors.value.push('Username is required')
+  }
+  if (username.value.length < 3) {
+    errors.value.push('Username must be at least 3 characters')
+  }
+  if (password.value === '') {
+    errors.value.push('Password is required')
+  }
+  if (password.value.length < 8) {
+    errors.value.push('Password must be at least 8 characters')
+  }
+
+  if (errors.value.length === 0) {
+    const formData = {
+      username: username.value,
+      password: password.value
+    }
+
+    axios
+      .post('/api/v1/token/login/', formData)
+      .then((response) => {
+        const token = response.data.auth_token
+        store.setToken(token)
+
+        axios.defaults.headers.common['Authorization'] = `Token ${token}`
+
+        localStorage.setItem('token', token)
+        router.push('/dashboard/my-account')
+      })
+      .catch((error) => {
+        if (error.response) {
+          for (const property in error.response.data) {
+            errors.value.push(`${property}: ${error.response.data[property]}`)
+          }
+          console.log(JSON.stringify(error.response.data))
+        } else if (error.message) {
+          errors.value.push('Something went wrong. Please try again')
+
+          console.log(JSON.stringify(error.message))
+        }
+      })
+  }
+}
+</script>
 
 <style></style>
