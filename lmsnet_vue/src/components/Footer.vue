19bbe8ebb6d26cbe6ea4122d74251beb300cd1ee
MinatoNami <minato.nami.dev@gmail.com>
2024-01-23 22:30:29 +0800

    Add basic pages and components

---
+++ lmsnet_vue/src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer class="footer">
+    <p class="has-text-centered">Copyright (c) 2024</p>
+  </footer>
+</template>
+
+<script setup lang="ts"></script>
+
+<style></style>
---
+++ lmsnet_vue/src/views/LoginView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="login">
+    <div class="hero is-info">
+      <div class="hero-body has-text-centered">
+        <h1 class="title">Log in</h1>
+      </div>
+    </div>
+    <section class="section">
+      <div class="container">
+        <div class="column">
+          <div class="column is-4 is-offset-4">
+            <form>
+              <div class="field">
+                <label>Email</label>
+                <div class="control"><input type="email" class="input" /></div>
+              </div>
+              <div class="field">
+                <label>Password</label>
+                <div class="control"><input type="password" class="input" /></div>
+              </div>
+              <div class="field">
+                <label>Confirm password</label>
+                <div class="control"><input type="password" class="input" /></div>
+              </div>
+              <div class="field">
+                <div class="control">
+                  <button class="button is-dark">Login</button>
+                </div>
+              </div>
+            </form>
+
+            <hr />
+
+            Or <router-link to="/sign-up">click here</router-link> to sign-up!
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style></style>
