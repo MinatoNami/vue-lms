ec1d777a091d84ff8feed732c320c2c531682709
MinatoNami <minato.nami.dev@gmail.com>
2024-02-06 14:42:43 +0800

    Add youtube embedded video

---
+++ lmsnet_vue/src/components/Video.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="video-container">
+    <iframe
+      width="100%"
+      height="400"
+      frameborder="0"
+      :src="'http://www.youtube.com/embed/' + youtube_id"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  youtube_id: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<style></style>
