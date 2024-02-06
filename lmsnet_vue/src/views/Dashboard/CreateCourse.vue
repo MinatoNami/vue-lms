30ba85d5bdc436904adb21f3630bf55cc3331985
Lionel <txg_ace@photons.com.tw>
2024-02-06 17:21:31 +0800

    Create author page

---
+++ lmsnet_vue/src/views/Dashboard/CreateCourse.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <div class="hero is-info">
+      <div class="hero-body has-text-centered">
+        <h1 class="title">Create course</h1>
+      </div>
+    </div>
+    <section class="section"></section>
+  </div>
+</template>
+<script setup lang="ts"></script>
+
+<style lang="less" scoped></style>
